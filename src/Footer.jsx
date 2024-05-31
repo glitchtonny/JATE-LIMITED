dbf692fc811603d78da2c79e5cbc2f5c90d92ed7
g;itchtonny <sirmwangitosh@gmail.com>
2024-05-31 23:01:45 +0300

    screenshot added

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="footer beige text-black text-center py-3">
       <div className="container">
-        <p>© 2024 Your Company Name. All Rights Reserved.</p>
+        <p>© 2024 JATE ltd. All Rights Reserved.</p>
         <p>
           <Link to="/" className="text-black">HOMEPAGE</Link> | 
           <Link to="/blog" className="text-black">BLOG</Link> | 

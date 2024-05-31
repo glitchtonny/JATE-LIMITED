5e46b08cdfdcfd79774b0f6375bdbb441b296b5d
g;itchtonny <sirmwangitosh@gmail.com>
2024-05-31 07:37:51 +0300

    push project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import DetailPage from './DetailPage';
+import Navbar from './NavBar';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<HomePage/>} />
+        <Route exact path="/:type/:id" element={<DetailPage/>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
